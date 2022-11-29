c2a242279f0301576ec5b2d2c3ffaa6a59b07d28
gabrielhsribeiro <ribeiro.gabrielhs@gmail.com>
2022-11-29 12:53:32 -0300

    update e delete

--- pages/api/task.ts
+++ pages/api/task.ts
@@ -6,13 +6,17 @@ import { DefaultMessageResponse } from '../../types/DefaultMessageResponse';
 import { Task } from '../../types/Task';
 
 const endpoint = async (req: NextApiRequest, res: NextApiResponse<DefaultMessageResponse | any>) => {
-    console.log(123);
+
     try {
         if (req.method === 'GET') {
             return getTasks(req,res);
         }else if (req.method === 'POST') {
             return saveTask(req,res);
-        }          
+        }else if (req.method === 'PUT') {
+            return updateTask(req,res);
+        }else if (req.method === 'DELETE') {
+            return deleteTask(req,res);
+        }
     } catch (e: any) {
         console.log('Ocorreu erro ao listar tarefas do usuário:', e);
         return res.status(500).json({ error: 'Ocorreu erro ao listar tarefas do usuário, tente novamente....' });
@@ -45,6 +49,68 @@ const saveTask = async(req: NextApiRequest, res: NextApiResponse<DefaultMessageR
 
     await TaskModel.create(task);
     return res.status(200).json({msg: 'Tarefa cadastrada com sucesso!'});
+}
+
+const updateTask = async(req: NextApiRequest, res: NextApiResponse<DefaultMessageResponse | any>) => {
+    if (!req.body) {
+        return res.status(400).json({ error: 'Favor informar os dados para atualizar' });
+    }
+
+    const task = req.body as Task;
+
+    if(!task.userId){
+        return res.status(400).json({ error: 'Usuario não encontrado' });
+    }
+
+    if(!task._id){
+        return res.status(400).json({ error: 'Necessário id da Tarefa a ser atualizada' });
+    }
+
+    const existsWithSameId = await TaskModel.find({_id: task._id});
+    if(!existsWithSameId){
+        return res.status(400).json({ error: 'Tarefa não existente' });
+    }
+
+    const id_task=task._id;
+    delete task._id;
+
+    if(!task.name || task.name.length < 2){
+        return res.status(400).json({ error: 'Nome não é válido' });
+    }
+
+    if(!task.finishPrevisionDate || task.finishPrevisionDate.length < 8){
+        return res.status(400).json({ error: 'Data de previsão não é válida' });
+    }
+
+    await TaskModel.updateOne({_id:id_task},task);
+    return res.status(200).json({msg: 'Tarefa atualizada com sucesso!'});
+}
+
+const deleteTask = async(req: NextApiRequest, res: NextApiResponse<DefaultMessageResponse | any>) => {
+    if (!req.body) {
+        return res.status(400).json({ error: 'Favor informar os dados para deletar' });
+    }
+
+    const task = req.body as Task;
+
+    if(!task.userId){
+        return res.status(400).json({ error: 'Usuario não encontrado' });
+    }
+
+    if(!task._id){
+        return res.status(400).json({ error: 'Necessário id da Tarefa a ser deletada' });
+    }
+
+    const existsWithSameId = await TaskModel.find({_id: task._id});
+    if(!existsWithSameId){
+        return res.status(400).json({ error: 'Tarefa não existente' });
+    }
+
+    const id_task=task._id;
+    delete task._id;
+
+    await TaskModel.deleteOne({_id:id_task});
+    return res.status(200).json({msg: 'Tarefa deletada com sucesso!'});
 }
 
 export default connectToDB(jwtValidator(endpoint));
